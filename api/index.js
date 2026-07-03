ab71660201109a70939cbeaf0a4451c6cef28341
Zaldy Bernabe | IMG Advocate <bernabezaldy@gmail.com>
2026-07-03 20:15:05 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.redirect(302, "https://m.me/YOUR_USERNAME");
+  res.redirect(302, "https://m.me/cm/AbbM8wDd01JOgyWZ/?send_source=cm:copy_invite_link");
 }
